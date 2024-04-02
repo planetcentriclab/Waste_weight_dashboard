08b91c7a3cdde3fd8d90ece395aa63c33bedf80c
mildphan <85720997+Phanisara@users.noreply.github.com>
2024-04-02 11:01:49 +0700

    Change the code for deploy in github pages

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './index.css'
 function App() {
   return (
     <div className='container'>
-      <Router basename={`${process.env.PUBLIC_URL}/`}>
+      <Router basename={`${process.env.PUBLIC_URL}`}>
         <Routes>
-            <Route path='/waste-weight'element= {<HomePage/>} />
-            <Route path='/waste-weight/machine-page'element= {<MachinePage/>} />
+            <Route path='/'element= {<HomePage/>} />
+            <Route path='/machine-page'element= {<MachinePage/>} />
           </Routes>
       </Router>
     </div>
