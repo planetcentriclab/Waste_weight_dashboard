475483a9fcb1cdfe05942efffe9614feaab5c92b
mildphan <85720997+Phanisara@users.noreply.github.com>
2024-04-02 10:31:30 +0700

    Change the path of url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/homePage';
 import MachinePage from './pages/machinePage';
 import './index.css'
@@ -8,9 +8,9 @@ function App() {
     <div className='container'>
       <Router>
         <Routes>
-            <Route path='https://planetcentriclab.github.io/Waste_weight_dashboard/'element= {<HomePage/>} />
-            <Route path='https://planetcentriclab.github.io/Waste_weight_dashboard/machine-page'element= {<MachinePage/>} />
-        </Routes>
+            <Route path='/waste-weight'element= {<HomePage/>} />
+            <Route path='/waste-weight/machine-page'element= {<MachinePage/>} />
+          </Routes>
       </Router>
     </div>
   );
