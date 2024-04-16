733d04dac6c173d9835ad9802a87e38427ff1245
mildphan <85720997+Phanisara@users.noreply.github.com>
2024-04-17 01:14:59 +0700

    Remove the navbar from the App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/homePage';
 import MachinePage from './pages/machinePage';
+import SideNavBar from './components/NavBar';
 import './index.css'
 
 function App() {
   return (
     <div className='container'>
       <Router basename={`${process.env.PUBLIC_URL}`}>
-        <Routes>
-            <Route path='/'element= {<HomePage/>} />
-            <Route path='/machine-page'element= {<MachinePage/>} />
+        <SideNavBar>
+          <Routes>
+              <Route path='/'element= {<HomePage/>} />
+              <Route path='/machine-page'element= {<MachinePage/>} />
           </Routes>
+        </SideNavBar>
       </Router>
     </div>
   );
