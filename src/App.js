904517a41a49143ac640f4049e4fe3f68499ec92
planetcentriclab <planetcentriclab@gmail.com>
2024-05-10 00:32:42 +0700

    Add new path of routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/homePage';
 import MachinePage from './pages/machinePage';
+import FactorPage from './pages/factorPage';
 import SideNavBar from './components/NavBar';
 import './index.css'
 
@@ -12,6 +13,7 @@ function App() {
           <Routes>
               <Route path='/'element= {<HomePage/>} />
               <Route path='/machine-page'element= {<MachinePage/>} />
+              <Route path='/emission-factor-page'element= {<FactorPage/>} />
           </Routes>
         </SideNavBar>
       </Router>
